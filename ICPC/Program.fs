c6e2d084f6d6ae70c96289a4bc56c07b01513a98
Robin <gvgollie4@gmail.com>
2019-04-15 17:44:19 +0200

    Error cases done

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -5,8 +5,23 @@ let commaSprinkler input =
     failwith "Not implemented"
 
 let rivers input =
-    failwith "Not implemented"
-
+    let lastIndex = input.ToString().Length-1
+    let stringInputArray = input.ToString().Split(' ')
+    let listString = stringInputArray|> Array.toList 
+    match input.ToString().Length>80, listString.Length < 2, input.ToString().Contains("  ") with
+    |true, _,_ -> None
+    |_,true, _ -> None
+    |_,_,true -> None
+    |_->    
+        match input.ToString().IndexOf(' ') = 0, input.ToString().LastIndexOf(' ') = lastIndex with
+        |true,_-> None
+        |_,true -> None
+        |_-> 
+            match input.ToString().Contains(","), input.ToString().Contains("!") with
+            |true,_ -> None
+            |_true -> None
+            |_ -> Some 1
+            
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
